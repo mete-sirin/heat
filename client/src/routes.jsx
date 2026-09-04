e1ed84a792a69eec22d2f2890025d65212670a74
Mete Şirin <metesirin.dev@gmail.com>
2026-09-04 18:13:41 +0300

    feat(client): add sign-in and sign-up authentication pages

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router";
+
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./ui/Layout";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import SignUpSuccess from "./features/Auth/SignUp/SignUpSuccess";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,15 @@ const router = createBrowserRouter([
     Component: LoginPage,
   },
 
+  {
+    path: "signup",
+    Component: SignUpPage,
+  },
+  {
+    path: "signup/success",
+
+    Component: SignUpSuccess,
+  },
   {
     path: "/",
     Component: Layout,
